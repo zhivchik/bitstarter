377bb6cbc3937f4d5f041cd3da57c29f7b22fef4
DJ Zhivchik <djzhivchik@gmail.com>
2013-07-04 21:21:10 +0000

    fix

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 var text = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
-  response.send(text);
+  response.send(text+'');
 });
 
 var port = process.env.PORT || 5000;
