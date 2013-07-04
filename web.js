8a3b231c12ed082ec1e982311222494dc17b36b4
DJ Zhivchik <djzhivchik@gmail.com>
2013-07-04 21:15:58 +0000

    web.js redo

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
+var text = fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
